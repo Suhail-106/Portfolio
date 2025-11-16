923d55a99af2938291b98f362309ad74ffeb3961
suhail hasan <97738180+Suhail-106@users.noreply.github.com>
2025-11-16 13:08:30 +0530

    Fix email host user domain in settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'companypvtltd35@gmail.com'
+EMAIL_HOST_USER = 'companypvtltd35@gmail.co'
 EMAIL_HOST_PASSWORD = 'qiplpmuhcjbvzjpy'
 
 EMAIL_SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
